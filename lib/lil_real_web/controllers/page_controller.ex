9d655835ebbb656add69ce9d7c0474a277c91c83
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-10-11 22:51:39 +0900

    ad

--- lib/lil_real_web/controllers/page_controller.ex
+++ lib/lil_real_web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule LilRealWeb.PageController do
   use LilRealWeb, :controller
+  require Logger
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  @impl true
+  def handle_event("new-room", _params, socket) do
+    Logger.info("clicked!")
+    {:noreply, socket}
+  end
+
 end
